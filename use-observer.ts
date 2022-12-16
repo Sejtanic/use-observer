c26ee6b8af71642f72b7128d9e00b49540e7c35c
Sejtanic <sejta@live.com>
2022-12-16 14:14:55 +0100

    first commit

---
+++ use-observer.ts
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+const useObserver = (
+  element: string,
+  eachtime: boolean,
+  addClass: string | null,
+  threshold: number,
+  callBack: Function = () => {}
+) => {
+  useEffect(() => {
+    const cards = document.querySelectorAll(element);
+    const observer = new IntersectionObserver(
+      (entry) => {
+        entry.forEach((entry) => {
+          if (entry.isIntersecting) {
+            addClass && entry.target.classList.add(addClass);
+            callBack();
+          } else if (eachtime) {
+            addClass && entry.target.classList.remove(addClass);
+          }
+        });
+      },
+      {
+        threshold,
+      }
+    );
+
+    cards.forEach((card) => observer.observe(card));
+  }, []);
+};
+export default useObserver;
